3d07faf4afca667f0b92b311ec6f695a1832ba5b
Sophia Riela <sophiarielaprofissional@gmail.com>
2026-04-08 10:55:04 -0300

    add gitignore

--- aulas/condicoes.py
+++ aulas/condicoes.py
@@ -1,6 +1,6 @@
-nome = str(input('Qual o seu nome? '))
+nome = str(input('Qual o seu nome?: '))
 if nome == 'Sophia':
     print('Que nome lindo você tem!')
 else:
-    print:('Seu nome é único!')
+    print('Seu nome é único!')
 print('Bom dia, {}!'.format(nome))
